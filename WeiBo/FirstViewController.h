f0a49416403e007347d307b75c0b365ebf4a3ed4
mac527 <mac527@imacdeiMac-33.local>
2015-09-09 10:30:03 +0800

    Initial Commit

---
+++ WeiBo/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  WeiBo
+//
+//  Created by mac527 on 15/9/9.
+//  Copyright (c) 2015年 mac527. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ WeiBo/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  WeiBo
+//
+//  Created by mac527 on 15/9/9.
+//  Copyright (c) 2015年 mac527. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
