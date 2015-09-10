7b1342ab7b081894f0f2125e38baadbf5a3b8abf
mac527 <mac527@imacdeiMac-33.local>
2015-09-10 22:30:23 +0800

    添加加号按钮

--- WeiBo/Classes/Main(主要)/View/CJTabBar.m
+++ WeiBo/Classes/Main(主要)/View/CJTabBar.m
@@ -12,10 +12,46 @@
 
 @property (nonatomic ,weak) CJTabBarButton *selectedButton;
 
+@property (nonatomic ,strong) NSMutableArray *allTabBarButton;
+
+@property (nonatomic ,weak) UIButton *plusButton;
+
 @end
 
 @implementation CJTabBar
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+
+    if (self == [super initWithFrame:frame]) {
+        
+        UIButton *plusButton = [[UIButton alloc] init];
+        
+        [plusButton setBackgroundImage:[UIImage imageWithName:@"tabbar_compose_button"] forState:UIControlStateNormal];
+        [plusButton setBackgroundImage:[UIImage imageWithName:@"tabbar_compose_button_highlighted"] forState:UIControlStateHighlighted];
+        
+        [plusButton setImage:[UIImage imageWithName:@"tabbar_compose_icon_add"] forState:UIControlStateNormal];
+        [plusButton setImage:[UIImage imageWithName:@"tabbar_compose_icon_add_highlighted"] forState:UIControlStateHighlighted];
+        plusButton.bounds = CGRectMake(0, 0, plusButton.currentBackgroundImage.size.width, 53);
+        
+        [self addSubview:plusButton];
+        self.plusButton = plusButton;
+        
+    }
+    
+    return  self;
+}
+
+
+- (NSMutableArray *)allTabBarButton
+{
+
+    if (_allTabBarButton == nil) {
+        
+        _allTabBarButton = [NSMutableArray array];
+    }
+    return _allTabBarButton;
+}
 
 
 /**
@@ -32,6 +68,7 @@
 
     [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchDown];
     
+    [self.allTabBarButton addObject:button];
 }
 
 
@@ -63,16 +100,23 @@
 
     [super layoutSubviews];
     
+    
+    self.plusButton.center = CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2);
+    
     CGFloat Y = 0;
     CGFloat W = kWidth / self.subviews.count;
     CGFloat H = self.frame.size.height;
-    for (int index = 0 ;index < self.subviews.count ; index ++)
+    
+
+    for (int index = 0 ;index < self.allTabBarButton.count ; index ++)
     {
 
-        CJTabBarButton *button = self.subviews[index];
-    
-        CGFloat X = W * index;
+        CJTabBarButton *button = self.allTabBarButton[index];
         
+        CGFloat X = W * index;
+        if (index > 1) {
+            X = W * (index + 1);
+        }
         button.frame = CGRectMake(X, Y, W, H);
      
         button.tag = 2015 + index;
